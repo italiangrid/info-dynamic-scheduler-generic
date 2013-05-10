280267fa63a8bc4b7abb87ecaaa01da65fc2a484
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2013-05-10 16:06:14 +0200

    RPMS from spec-file

--- setup.py
+++ setup.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python
 
+import sys, os, os.path, shlex, subprocess
+from subprocess import Popen as execScript
 from distutils.core import setup
-import ConfigParser
+from distutils.command.bdist_rpm import bdist_rpm as _bdist_rpm
 
-pkg_version = '0.0.0'
-try:
-    parser = ConfigParser.ConfigParser()
-    parser.read('setup.cfg')
-    pkg_version = parser.get('global','pkgversion')
-except:
-    pass
+pkg_name = 'dynsched-generic'
+pkg_version = '2.5.2'
+pkg_release = '1'
+
+source_items = "setup.py src tests"
+
+class bdist_rpm(_bdist_rpm):
+
+    def run(self):
+
+        topdir = os.path.join(os.getcwd(), self.bdist_base, 'rpmbuild')
+        builddir = os.path.join(topdir, 'BUILD')
+        srcdir = os.path.join(topdir, 'SOURCES')
+        specdir = os.path.join(topdir, 'SPECS')
+        rpmdir = os.path.join(topdir, 'RPMS')
+        srpmdir = os.path.join(topdir, 'SRPMS')
+        
+        cmdline = "mkdir -p %s %s %s %s %s" % (builddir, srcdir, specdir, rpmdir, srpmdir)
+        execScript(shlex.split(cmdline), stdout=sys.stdout, stderr=sys.stderr).communicate()
+        
+        cmdline = "tar -zcf %s %s" % (os.path.join(srcdir, pkg_name + '.tar.gz'), source_items)
+        execScript(shlex.split(cmdline), stdout=sys.stdout, stderr=sys.stderr).communicate()
+        
+        specOut = open(os.path.join(specdir, pkg_name + '.spec'),'w')
+        cmdline = "sed -e 's|@PKGVERSION@|%s|g' -e 's|@PKGRELEASE@|%s|g' project/%s.spec.in" % (pkg_version, pkg_release, pkg_name)
+        execScript(shlex.split(cmdline), stdout=specOut, stderr=sys.stderr).communicate()
+        specOut.close()
+        
+        cmdline = "rpmbuild -ba --define '_topdir %s' %s.spec" % (topdir, os.path.join(specdir, pkg_name))
+        execScript(shlex.split(cmdline), stdout=sys.stdout, stderr=sys.stderr).communicate()
 
 setup(
       name='dynsched-generic',
@@ -26,7 +51,8 @@ Output is in a format compatible with the GIP of gLite.''',
       package_dir = {'': 'src'},
       data_files=[
                   ('usr/libexec', ['src/lcg-info-dynamic-scheduler'])
-                 ]
+                 ],
+      cmdclass={'bdist_rpm': bdist_rpm}
      )
 
 
